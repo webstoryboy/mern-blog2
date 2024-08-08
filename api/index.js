7486ee3c914d209dbf843a689ac425ccd5ab5d6c
웹스토리보이 <webstupids@gmail.com>
2024-08-08 17:44:10 +0900

    dd

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -23,6 +24,8 @@ mongoose
         console.log(err);
     });
 
+const __dirname = path.resolve();
+
 app.listen(3000, () => {
     console.log("포트 3000에서 서버가 작동하고 있습니다.");
 });
@@ -35,6 +38,12 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
